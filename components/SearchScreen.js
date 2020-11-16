65b25ccb439679b634fef3acea4f19b84447e328
Weronika <werawera112@gmail.com>
2020-11-16 23:10:25 +0100

    add navigation to SearchScreen and import to SearchStack

--- components/SearchScreen.js
+++ components/SearchScreen.js
@@ -1,11 +1,75 @@
-import React from 'react';
-import { View } from 'react-native';
-import VideoCardSearch from './VideoCardSearch';
+import React, {useState,useContext} from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import CardSearch from './CardSearch';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import Modal from 'react-native-modal';
+import ScreenDialog from './TopNavigationDialogs/ScreenDialog';
+import {AppContext} from './AppContext'
 
-const SearchScreen = () => {
+const SearchScreen = ({navigation}) => {
+  const [modalScreenVisible, setModalScreenVisible] = useState(false);
+
+  const { titleNavigation, setModalSearchVisible, searchQuery, setSearchQuery } = useContext(
+    AppContext
+  );
+
   return (
     <View>
-      <VideoCardSearch />
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          height: 60,
+          marginTop: 15
+        }}
+      >
+        <MaterialCommunityIcons
+          name='arrow-left'
+          size={24}
+          color='black'
+          style={{ marginLeft: 20 }}
+          onPress={() => {setModalSearchVisible(false), setSearchQuery('')}}
+        />
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Text style={{ left: 8, fontSize: 16 }}>{searchQuery}</Text>
+        </TouchableOpacity>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            right: 10,
+            position: 'absolute',
+            width: '25%'
+          }}
+        >
+          <MaterialCommunityIcons
+            name='close'
+            size={28}
+            color='black'
+            onPress={() => { navigation.goBack(), setSearchQuery('')}}
+          />
+          <MaterialCommunityIcons
+            name='monitor-screenshot'
+            size={28}
+            color='black'
+            onPress={() => setModalScreenVisible(!modalScreenVisible)}
+          />
+        </View>
+        <View>
+          <Modal
+            isVisible={modalScreenVisible}
+            style={{
+              justifyContent: 'flex-end',
+              margin: 0
+            }}
+          >
+            <ScreenDialog
+              close={() => setModalScreenVisible(!modalScreenVisible)}
+            />
+          </Modal>
+        </View>
+      </View>
+      <CardSearch />
     </View>
   );
 };
--- components/SearchStack.js
+++ components/SearchStack.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import SearchDialog from './TopNavigationDialogs/SearchDialog';
-import Test from './Test';
+import SearchScreen from './SearchScreen';
 
 const Stack = createStackNavigator();
 
@@ -13,7 +13,7 @@ const SearchStack = () => {
       }}
     >
       <Stack.Screen name='Wyszukaj' component={SearchDialog} />
-      <Stack.Screen name='Test' component={Test} />
+      <Stack.Screen name='SearchScreen' component={SearchScreen} />
     </Stack.Navigator>
   );
 };
